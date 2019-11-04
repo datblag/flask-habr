19c0260ffeb75576cb67d4a01b5daf151a4454ad
an <d@mail.ru>
2019-11-04 13:06:32 +0900

    ok

--- habr_microblog/__init__.py
+++ habr_microblog/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-
+from habr_microblog_config import Config
 app = Flask(__name__)
-
-app.config.from_object('config')
+app.config.from_object(Config)
 
 from habr_microblog import routes
+
+
