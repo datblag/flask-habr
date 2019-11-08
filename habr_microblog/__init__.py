afaff6777a5f952110d08c0088b9c9e4d51db9d8
an <d@mail.ru>
2019-11-08 21:35:47 +0900

    ok

--- habr_microblog/__init__.py
+++ habr_microblog/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from habr_microblog_config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from habr_microblog import routes
+from habr_microblog import routes, models
 
 
