148999b919108a7eaabec2c99be04034788ee996
an <d@mail.ru>
2020-04-18 08:44:01 +0900

    ok

--- habr_microblog.py
+++ habr_microblog.py
@@ -1,3 +1,11 @@
 from habr_microblog import app
+import cherrypy
 
-app.run()
+
+cherrypy.tree.graft(app, '/blog/')
+cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                        'server.socket_port': 5000,
+                        'engine.autoreload.on': False,
+                        })
+cherrypy.engine.start()
+# app.run()
